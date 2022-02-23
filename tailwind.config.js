5b032c70ce03e0a7271ad04bf484aae01f741c5b
Piotr Bęc <becpiotr@gmail.com>
2022-02-23 12:21:16 +0100

    added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/!(*.spec).{html,scss,ts}"
+  ],
+  theme: {
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
