70f4b675be8c61735d4b584f8da1fe4bd9d55e5f
Piotr Bęc <becpiotr@gmail.com>
2022-02-23 12:15:13 +0100

    added Font Awesome

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,5 @@
-import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
-import { Inject, NgModule, Optional, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformServer } from '@angular/common';
+import { Inject, NgModule, PLATFORM_ID } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
@@ -7,10 +7,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { DomSanitizer } from '@angular/platform-browser';
 import { FooterComponent } from './components/footer/footer.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
 
 const MATERIAL_MODULES = [MatMenuModule, MatToolbarModule, MatIconModule, MatButtonModule];
 
-const SHARED_MODULES = [CommonModule, ...MATERIAL_MODULES];
+const SHARED_MODULES = [CommonModule, ...MATERIAL_MODULES, FontAwesomeModule];
 
 const SHARED_COMPONENTS = [HeaderComponent, FooterComponent];
 
@@ -23,7 +25,8 @@ export class SharedModule {
   constructor(
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer,
-    @Inject(PLATFORM_ID) private platformId: string
+    @Inject(PLATFORM_ID) private platformId: string,
+    faIconLibrary: FaIconLibrary
   ) {
     const domain = isPlatformServer(platformId) ? 'http://localhost:4200/' : '';
     this.matIconRegistry.addSvgIcon(
@@ -38,5 +41,7 @@ export class SharedModule {
       'lang_de',
       this.domSanitizer.bypassSecurityTrustResourceUrl(domain + 'assets/langs-svg/de.svg')
     );
+
+    faIconLibrary.addIcons(faAngular);
   }
 }
